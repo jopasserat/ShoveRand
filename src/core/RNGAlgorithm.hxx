532b03715c304b11d837560356d66e2a2ecf890d
jH@CKtheRipper <passerat@isima.fr>
2011-11-27 19:04:40 +0000

    Forced presence of RNG::release __host__ method in every RNG implementation.

--- src/core/RNGAlgorithm.hxx
+++ src/core/RNGAlgorithm.hxx
@@ -43,6 +43,7 @@ public:
 	// concept requirements
 	BOOST_CONCEPT_USAGE(RNGAlgorithm) {
       al_.init(42);                // require Algo<T>::init()
+      al_.release();					// require Algo<T>::release()
 		value_ = al_.next();       // require T Algo<T>::next() 
       same_type(ss_, al_.ss_);   // require Algo<T>::ss_ to be of SeedStatus<Algo> type
       //same_type(ps_, al_.ps_);   // require Algo<T>::ps_ to be of ParameterizedStatus<Algo> type
