bfa74761c89bad4fb404aae31f2ea275938f31dd
jH@CKtheRipper <passerat@isima.fr>
2012-06-30 20:02:18 +0200

    Cleaned test file

--- test/test-shoverand.cu
+++ test/test-shoverand.cu
@@ -8,9 +8,7 @@
 #include <cstdlib>
 
 #include <cuda.h>
-#include <cutil.h>
-#include <cutil_inline_runtime.h>
-
+#include <shoverand/util/myCutil.h>
 
 #include <iostream> // debug purposes
 
@@ -20,34 +18,20 @@ using shoverand::RNG;
 using shoverand::MRG32k3a;
 using shoverand::TinyMT;
 
+typedef RNG < float, MRG32k3a > rng_type;
 
-/** Kernel testing MRG32k3a implementation */
-//__global__ void testMRG32k3a(double* ddata,  ParameterizedStatusType* param) {
-__global__ void testMRG32k3a(double* ddata) {
 
-	// this call might not work with devices of
-	// compute capability < 2.x
-	RNG < float, MRG32k3a > 	rng;
-	//rng.init();
-
-   // TODO old devices compliant version
-   
-	ddata[blockDim.x * blockIdx.x + threadIdx.x] = rng.next();
-   __syncthreads();
-}
-
-/** Kernel testing TinyMT implementation */
-__global__ void testTinyMT(double* ddata) {
+/** Kernel testing PRNG implementations */
+__global__ void testPRNG(double* ddata) {
 
 	// this call might not work with devices of
 	// compute capability < 2.x
-	RNG < float, TinyMT > 	rng;
-	//rng.init();
+	rng_type 	rng;
 
-   // TODO old devices compliant version
+	// TODO old devices compliant version
 
 	ddata[blockDim.x * blockIdx.x + threadIdx.x] = rng.next();
-   __syncthreads();
+	__syncthreads();
 }
 
 
@@ -57,21 +41,20 @@ __global__ void testVariateGenerator(double* ddata) {
 
 	// this call might not work with devices of
 	// compute capability < 2.x
-	typedef RNG < float, MRG32k3a >  				 randomengine;
-	typedef shoverand::uniform_01< float, float>      distribution;
+	typedef rng_type				    				 randomengine;
+	typedef shoverand::uniform_01< float, float>         distribution;
 
 	randomengine 	rng;
-	//rng.init();
 
 	distribution myDistribution;
 
 	shoverand::variate_generator
-         <
-         randomengine,
-         distribution
-         >
-         myVariateGenerator(rng, myDistribution);
-   
+	<
+	randomengine,
+	distribution
+	>
+	myVariateGenerator(rng, myDistribution);
+
 	ddata[blockDim.x * blockIdx.x + threadIdx.x] = myVariateGenerator();
 }
 
@@ -84,90 +67,86 @@ int main(int, char **) {
 	cudaMemGetInfo(&memFree, &memTotal);
 	std :: cerr << "Available device memory at the beginning: " << memFree << "/" << memTotal << std::endl;
 
-   int block_num = 3;
-   int thread_num = 512;
-   int data_size = block_num * thread_num * sizeof(double);
-   
-   double* d_data;
-   double* h_data;
-   cudaError_t e;
-   float gputime;
+	int block_num = 3;
+	int thread_num = 512;
+	int data_size = block_num * thread_num * sizeof(double);
 
-   
-   // create timers 
-   cudaEvent_t start;
-   cudaEvent_t stop;
-   cudaEventCreate(&start);
-   cudaEventCreate(&stop);
-   
-   
-   // allocate memory for data on device
-   cutilSafeCall( cudaMalloc((void**) &d_data, data_size) );
-   cutilSafeCall( cudaMemset(d_data, 0, data_size) );
+	double* d_data;
+	double* h_data;
+	cudaError_t e;
+	float gputime;
 
 
-   if (cudaGetLastError() != cudaSuccess) {
-      std::cerr << "error has occured before kernel call." << std::endl;
-      exit(1);
-   }
-   
-	
-	//RNG< float, MRG32k3a > ::init(block_num);
-   RNG< float, TinyMT > ::init(block_num);
-	
-   cudaEventRecord(start, 0);
-   
+	// create timers
+	cudaEvent_t start;
+	cudaEvent_t stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
-   // kernel call
-   //testMRG32k3a<<< block_num, thread_num >>>(d_data, status_device);
-	//testVariateGenerator<<< block_num, thread_num >>>(d_data, status_device);
-	//testMRG32k3a<<< block_num, thread_num >>>(d_data);
-//	testVariateGenerator<<< block_num, thread_num >>>(d_data);
-	testTinyMT<<< block_num, thread_num >>>(d_data);
-   
-   cudaEventRecord(stop, 0);
-   cudaEventSynchronize(stop);
-   
-   e = cudaGetLastError();
-   if (e != cudaSuccess) {
-      std::cerr << "failure in kernel call.\n" << cudaGetErrorString(e) << std::endl;
-      exit(2);
-   }
-   
-   // allocate memory to get results back on the host
-   h_data = new double[data_size];
-   
-   if (h_data == NULL) {
-      std::cerr << "failure in allocating host memory for output data." << std::endl;
-      exit(3);
-   }
-   
-   cutilSafeCall(
-                 cudaMemcpy(h_data,
-                            d_data,
-                            data_size,
-                            cudaMemcpyDeviceToHost));
-   cudaEventElapsedTime(&gputime, start, stop);
-   
-   
-   for (int i = 0; i < block_num * thread_num; ++i) {
+
+	// allocate memory for data on device
+	myCutilSafeCall( cudaMalloc((void**) &d_data, data_size) );
+	myCutilSafeCall( cudaMemset(d_data, 0, data_size) );
+
+
+	if (cudaGetLastError() != cudaSuccess) {
+		std::cerr << "error has occured before kernel call." << std::endl;
+		exit(1);
+	}
+
+	// init step from host side
+	rng_type ::init(block_num);
+
+	cudaEventRecord(start, 0);
+
+
+	// --- kernel calls ---
+	//	testVariateGenerator<<< block_num, thread_num >>>(d_data);
+	testPRNG<<< block_num, thread_num >>>(d_data);
+
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+
+	e = cudaGetLastError();
+	if (e != cudaSuccess) {
+		std::cerr << "failure in kernel call.\n" << cudaGetErrorString(e) << std::endl;
+		exit(2);
+	}
+
+	// allocate memory to get results back on the host
+	h_data = new double[data_size];
+
+	if (h_data == NULL) {
+		std::cerr << "failure in allocating host memory for output data." << std::endl;
+		exit(3);
+	}
+
+	myCutilSafeCall(
+			cudaMemcpy(h_data,
+					d_data,
+					data_size,
+					cudaMemcpyDeviceToHost));
+	cudaEventElapsedTime(&gputime, start, stop);
+
+
+	for (int i = 0; i < block_num * thread_num; ++i) {
 		std::cout << "h_data[" << i << "] = " << h_data[i] << std::endl;
-   }
-   
-   
-   std::cout << "generated numbers: " << thread_num * block_num << std::endl;
-   std::cout << "Processing time: " << gputime << " (ms)" << std::endl;
-   std::cout << "Samples per second: " << (thread_num * block_num) / (gputime * 0.001) << std::endl; 
-   
-   //free memory
-   cudaEventDestroy(start);
-   cudaEventDestroy(stop);
+	}
 
-//	RNG< float, MRG32k3a > :: release();
-   	RNG< float, TinyMT > :: release();
-	
-   delete [] h_data;
-   cutilSafeCall(cudaFree(d_data));   
+
+	std::cout << "generated numbers: " << thread_num * block_num << std::endl;
+	std::cout << "Processing time: " << gputime << " (ms)" << std::endl;
+	std::cout << "Samples per second: " << (thread_num * block_num) / (gputime * 0.001) << std::endl;
+
+	//free memory
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	// release memory allocated on device from host
+	rng_type :: release();
+
+	delete [] h_data;
+	myCutilSafeCall(cudaFree(d_data));
 
 	cudaMemGetInfo(&memFree, &memTotal);
 	std :: cerr << "Available device memory at the end: " << memFree << "/" << memTotal << std::endl;
