80e45c761101342c7cb23329edec72efaf92aad0
jH@CKtheRipper <passerat@isima.fr>
2012-07-02 10:32:54 +0000

    Fixed concept checking on static methods and correctly indented RNGAlgorithm.hxx

--- src/core/RNGAlgorithm.hxx
+++ src/core/RNGAlgorithm.hxx
@@ -17,39 +17,40 @@
 
 /** Class describing the RNGAlgorithm policy. */
 template < 
-         class T,
-         template <class> class Algo
-         >
-class RNGAlgorithm {
+class T,
+      template <class> class Algo
+      >
+      class RNGAlgorithm {
 
-private:
-   Algo<T> al_;
-   T value_;
-   SeedStatus< Algo > ss_;
-   ParameterizedStatus< Algo >* ps_;
-   
-   
-private:
-   template <class U>
-   void same_type(const U&, const U&);
-   
-   
-public:
-	
-	// TODO add variate_generator must_haves
-	// TODO force RNG implementations to provide SeedStatusType and ParameterizedStatusType typdefs
-	
-	
-	// concept requirements
-	BOOST_CONCEPT_USAGE(RNGAlgorithm) {
-      al_.init(42);                // require Algo<T>::init()
-      al_.release();					// require Algo<T>::release()
-	  value_ = al_.next();       // require T Algo<T>::next()
+         private:
+            Algo<T> al_;
+            T value_;
+            SeedStatus< Algo > ss_;
+            ParameterizedStatus< Algo >* ps_;
 
-	  same_type(ss_, al_.ss_);   // require Algo<T>::ss_ to be of SeedStatus<Algo> type
-      same_type(ps_, al_.ps_);   // require Algo<T>::ps_ to be of ParameterizedStatus<Algo> type
-   }
 
-};
+         private:
+            template <class U>
+               void same_type(const U&, const U&);
+
+
+         public:
+
+            // TODO add variate_generator must_haves
+            // TODO force RNG implementations to provide SeedStatusType and ParameterizedStatusType typdefs
+
+
+            // concept requirements
+            BOOST_CONCEPT_USAGE(RNGAlgorithm) {
+               Algo<T>::init(42);                // require Algo<T>::init()
+               Algo<T>::release();					// require Algo<T>::release()
+               value_ = al_.next();       // require T Algo<T>::next()
+
+               same_type(ss_, al_.ss_);   // require Algo<T>::ss_ to be of SeedStatus<Algo> type
+               same_type(ps_, al_.ps_);   // require Algo<T>::ps_ to be of ParameterizedStatus<Algo> type
+            }
+
+      };
 
 #endif // RNG_ALGORITHM_HXX
+
